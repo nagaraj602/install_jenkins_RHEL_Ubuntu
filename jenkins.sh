915d8eead6eb2f3413dc183699327327872a840c
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-02-27 21:41:09 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -85,7 +85,7 @@ elif [ "$distro" == "ubuntu" ]; then
     https://pkg.jenkins.io/debian-stable binary/" | \
     sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-    
+    sudo apt-get update -y > /dev/null
     sudo apt-get install fontconfig openjdk-25-jre -y > /dev/null
     sudo apt install jenkins -y > /dev/null
 
